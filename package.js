3cdb1423fc8c11372125433979b2d4533cf369cf
Robert David McLeod <robertdavid01@gmail.com>
2018-03-28 13:49:27 -0700

    Resolve style lib conflicts

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rd010:bootstrap-master-modal',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'Dynamic Modals. Use AutoForm, Bootstrap v 3/4, & existing templates for lightboxes, dialogues etc.',
   // URL to the Git repository containing the source code for this package.
